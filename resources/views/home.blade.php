bf0ca03e9b0a6c8332f0e047a07a866d6431754c
Rizki Dendy <rizkidendy59@gmail.com>
2020-05-15 17:22:05 +0700

    First templating

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+   <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          <div class="section-header">
+            <h1>Dashboard Sistem Informasi Kepegawaian</h1>
+          </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+          <div class="section-body">
+            <h2 class="section-title">Selamat datang di menu Dashboard, Admin !</h2>
+              <p class="section-lead">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+              </p>
+              <!-- Your content goes here -->
 
-                    You are logged in!
-                </div>
+
+              <div class="card">
+              <div class="card-header">
+                <h4>Example Card</h4>
+              </div>
+              <div class="card-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+              <div class="card-footer bg-whitesmoke">
+                This is card footer
+              </div>
             </div>
-        </div>
-    </div>
-</div>
+            
+          </div>
+        </section>
+      </div>
 @endsection
